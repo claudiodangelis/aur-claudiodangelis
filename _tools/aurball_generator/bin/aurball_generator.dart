e982f86cc72b92e4ca1123c016f8175c4f49033a
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-09-12 23:16:43 +0200

    Generator almost completed

--- _tools/aurball_generator/bin/aurball_generator.dart
+++ _tools/aurball_generator/bin/aurball_generator.dart
@@ -5,9 +5,18 @@ import 'dart:convert' show JSON;
 import 'package:aurball_generator/sdk.dart' as sdk;
 import 'package:aurball_generator/editor.dart' as editor;
 
+import 'package:html5lib/parser.dart' show parse;
+import 'package:html5lib/dom.dart';
+
 final String USER_HOME = "/home/dawson";
-final String VERSION_URI = "http://gsdview.appspot.com/dart-archive/channels/dev/release/latest/VERSION";
+final String VERSION_URI =
+    "http://gsdview.appspot.com/dart-archive/channels/dev/release/latest/VERSION";
 final String VERSION_FILE = USER_HOME + "/.dartAurballGenerator";
+
+final String EDITOR_URI =
+    "http://gsdview.appspot.com/dart-archive/channels/dev/release/latest/editor/";
+final String SDK_URI =
+    "http://gsdview.appspot.com/dart-archive/channels/dev/release/latest/sdk/";
 
 main() {
   // TODO: use a logger
@@ -20,12 +29,60 @@ main() {
     if (isNew(version["revision"])) {
       print("Starting the process");
       // Get:
-      // - Editor md5sum64
-      // - Editor md5sum32
-      // - SDK md5sum64
-      // - SDK md5sum32
-      //
-      // TODO: complete
+      // Editor data:
+      http.get(EDITOR_URI).then((http.Response resp) {
+        String md5sum32;
+        String md5sum64;
+        Document editorHtml = parse(resp.body);
+        List<Element> anchors = editorHtml.querySelectorAll('html a');
+        anchors.forEach((elem) {
+          switch (elem.text) {
+            case "darteditor-linux-ia32.zip":
+              // - Editor md5sum32
+              md5sum32 =
+                  elem.parent.parent.children[3].text.replaceAll('"', "");
+              break;
+
+            case "darteditor-linux-x64.zip":
+              // - Editor md5sum64
+              md5sum64 =
+                  elem.parent.parent.children[3].text.replaceAll('"', "");
+              break;
+          }
+        });
+
+        makeAurball('editor', editor.getPkgBuild(version["version"],
+          version["revision"], md5sum64,md5sum32), '/tmp');
+
+      });
+
+      // SDK data
+      http.get(SDK_URI).then((http.Response resp) {
+        String md5sum32;
+        String md5sum64;
+        Document editorHtml = parse(resp.body);
+        List<Element> anchors = editorHtml.querySelectorAll('html a');
+        anchors.forEach((elem) {
+          switch (elem.text) {
+            case "dartsdk-linux-ia32-release.zip":
+              // - SDK md5sum32
+              md5sum32 =
+                  elem.parent.parent.children[3].text.replaceAll('"', "");
+              break;
+
+            case "dartsdk-linux-x64-release.zip":
+              // - SDK md5sum64
+              md5sum64 =
+                  elem.parent.parent.children[3].text.replaceAll('"', "");
+              break;
+          }
+        });
+
+        makeAurball('sdk', sdk.getPkgBuild(version["version"],
+          version["revision"], md5sum64,md5sum32), '/tmp');
+
+      });
+      // TODO: create/update ~/.dartAurballGenerator
     } else {
       print("Dart is up-to-date");
       // Quit
@@ -37,4 +94,17 @@ bool isNew(String revision) {
   File f = new File(VERSION_FILE);
   bool exists = f.existsSync();
   return (exists && revision != f.readAsStringSync().trim() || !exists);
+}
+
+void makeAurball(String pkgname, String pkgbuild, String path) {
+  print("Making aurball for $pkgname in $path");
+  String pkgbuildPath = path + "/PKGBUILD_" + pkgname;
+  File f = new File(pkgbuildPath);
+  f.writeAsStringSync(pkgbuild);
+
+  // FIXME ASAP: the actual tarball is generated in the cwd, not cool
+  Process.run('mkaurball', ['-p', pkgbuildPath]).then((_) {
+    f.deleteSync();
+    print("Completed making aurball for $pkgname");
+  });
 }
--- _tools/aurball_generator/lib/sdk.dart
+++ _tools/aurball_generator/lib/sdk.dart
@@ -1,91 +1,50 @@
 library sdk;
 
 String getPkgBuild(String version, String revision, String md5sum64, String md5sum32) {
+  version = version.replaceAll("-", "_");
   return """
-# Maintainer : Claudio d'Angelis <claudiodangelis at gmail dot com>
+# Maintainer: Claudio d'Angelis <claudiodangelis at gmail dot com>
 
-pkgname=dart-editor-dev
-_pkgname=DartEditor
+pkgname=dart-sdk-dev
 pkgver=$version
 pkgrel=1
-pkgdesc="Editor and tools for the Dart programming language."
+pkgdesc="Tools and libraries for Dart development (development channel)"
 arch=(i686 x86_64)
-url="http://www.dartlang.org/docs/editor/getting-started"
-license=(EPL)
-conflicts=('dart-editor')
-provides=('dart-editor')
-
-
-depends=('java-runtime>=6' 'gtk2' 'desktop-file-utils' 'gconf' 'libudev.so.0' 'libgcrypt15')
+url="https://www.dartlang.org/tools/sdk/"
+license=('BSD 3-Clause')
+provides=('dart-sdk')
+conflicts=('dart-sdk' 'dart-editor')
 makedepends=('unzip')
-backup=(opt/dart-editor/\${_pkgname}.ini)
 
 if [[ \${CARCH} = x86_64 ]]; then
-    source=("https://storage.googleapis.com/dart-archive/channels/dev/release/$revision/editor/darteditor-linux-x64.zip")
-    md5sums=("$md5sum64")
+  source=("https://storage.googleapis.com/dart-archive/channels/dev/release/$revision/sdk/dartsdk-linux-x64-release.zip")
+  md5sums=("$md5sum64")
 else
-    source=("https://storage.googleapis.com/dart-archive/channels/dev/release/$revision/editor/darteditor-linux-ia32.zip")
-    md5sums=("$md5sum32")
+  source=("https://storage.googleapis.com/dart-archive/channels/dev/release/$revision/sdk/dartsdk-linux-ia32-release.zip")
+  md5sums=("$md5sum32")
 fi
 
-build() {
-  msg2 "Generate desktop application entry..."
-  cat > "\${srcdir}"/dart-editor.desktop << EOF
-[Desktop Entry]
-Version=\${pkgver}
-Encoding=UTF-8
-Name=Dart editor
-Comment=\${pkgdesc}
-Exec=/usr/bin/dart-editor
-Icon=/opt/dart-editor/icon.xpm
-Terminal=false
-Type=Application
-Categories=Development;
-EOF
-}
-
 package() {
-  msg2 "Install the assembly at /opt/dart-editor..."
-  install -dm755           "\${pkgdir}"/opt/dart-editor
-  cp -a "\${srcdir}"/dart/* "\${pkgdir}"/opt/dart-editor
 
-  msg2 "Fix permissions"
-  chmod -R +r "\${pkgdir}"/opt/dart-editor
-  chmod -R +r "\${pkgdir}"/opt/dart-editor/dart-sdk
-  chmod -R +r "\${pkgdir}"/opt/dart-editor/chromium
-  chmod +x "\${pkgdir}"/opt/dart-editor/dart-sdk/bin/*
+    cd \$pkgdir
 
-  msg2 "Install links to the executables in /usr/bin..."
-  install -dm755                    "\${pkgdir}"/usr/bin
-  ln -s /opt/dart-editor/\${_pkgname} "\${pkgdir}"/usr/bin/dart-editor
-  ln -s /opt/dart-editor/\${_pkgname} "\${pkgdir}"/usr/bin/\${_pkgname}
+    mkdir -p opt/dart-sdk usr/bin usr/share/licenses/dart-sdk
+    cp -r \$srcdir/dart-sdk/* opt/dart-sdk
 
-  msg2 "Install links to the documentation resources in /usr/share/doc/dart-editor..."
-  install -dm755                    "\${pkgdir}"/usr/share/doc/dart-editor
-  ln -s /opt/dart-editor/samples     "\${pkgdir}"/usr/share/doc/dart-editor/
-  ln -s /opt/dart-editor/about.html  "\${pkgdir}"/usr/share/doc/dart-editor/
-  ln -s /opt/dart-editor/about_files "\${pkgdir}"/usr/share/doc/dart-editor/
+    find . -type f -exec chmod 644 "{}" \;
+    find . -type d -exec chmod 755 "{}" \;
 
-  msg2 "Install link to the config file in /etc..."
-  install -dm755                        "\${pkgdir}"/etc
-  ln -s /opt/dart-editor/\${_pkgname}.ini "\${pkgdir}"/etc/dart-editor.ini
+    chmod +x opt/dart-sdk/bin/*
 
-  msg2 "Install desktop application entry in /usr/share/applications..."
-  install -Dm644 "\${srcdir}"/dart-editor.desktop "\${pkgdir}"/usr/share/applications/dart-editor.desktop
+    ln -s /opt/dart-sdk/bin/pub            usr/bin/pub
+    ln -s /opt/dart-sdk/bin/dart           usr/bin/dart
+    ln -s /opt/dart-sdk/bin/docgen         usr/bin/docgen
+    ln -s /opt/dart-sdk/bin/dart2js        usr/bin/dart2js
+    ln -s /opt/dart-sdk/bin/dartfmt        usr/bin/dartfmt
+    ln -s /opt/dart-sdk/bin/dartanalyzer   usr/bin/dartanalyzer
 
-  msg2 "Linking binaries"
-  cd \$pkgdir
-
-  mkdir -p opt/dart-editor/dart-sdk/bin usr/bin
-  chmod +x opt/dart-editor/dart-sdk/bin/*
-  ln -s /opt/dart-editor/dart-sdk/bin/pub            usr/bin/pub
-  ln -s /opt/dart-editor/dart-sdk/bin/dart           usr/bin/dart
-  ln -s /opt/dart-editor/dart-sdk/bin/docgen         usr/bin/docgen
-  ln -s /opt/dart-editor/dart-sdk/bin/dart2js        usr/bin/dart2js
-  ln -s /opt/dart-editor/dart-sdk/bin/dartfmt        usr/bin/dartfmt
-  ln -s /opt/dart-editor/dart-sdk/bin/dartanalyzer   usr/bin/dartanalyzer
+    sed -n '2,4p' opt/dart-sdk/bin/dart2js > usr/share/licenses/dart-sdk/license
 
 }
-
 """;
 }
